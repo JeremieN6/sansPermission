bb5c917bbcc1707de08e1e29f02f88db954a3ef3
Jérémie NGOYI <47412701+JeremieN6@users.noreply.github.com>
2025-04-18 23:28:50 +0000

    ✨ Ajout de la page d'accueil du quiz et de la barre de navigation

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Answers;
 use App\Entity\Quizzes;
+use App\Repository\QuizzesRepository;
 use App\Repository\UserScoresRepository;
 use App\Service\ScoreService;
 use Doctrine\ORM\EntityManagerInterface;
@@ -11,9 +12,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\YouTubeScriptService;
 
 class QuizController extends AbstractController
 {
+    private YouTubeScriptService $YouTubeScriptService;
+
+    // Injecter YouTubeScriptService dans le constructeur
+    public function __construct(YouTubeScriptService $YouTubeScriptService)
+    {
+        $this->YouTubeScriptService = $YouTubeScriptService;
+    }
 
     #[Route('/quiz/{id}/submit', name: 'quiz_submit', methods: ['POST'])]
     public function submitQuiz(
@@ -40,5 +49,25 @@ class QuizController extends AbstractController
         $this->addFlash('success', "Tu as obtenu $correctAnswers bonnes réponses !");
 
         return $this->redirectToRoute('quiz_results', ['id' => $quiz->getId()]);
+    }
+
+    #[Route('/quiz-home', name: 'quiz_home')]
+    public function indexQuiz(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserScoresRepository $userScoresRepository,
+        QuizzesRepository $quizzesRepository
+    ){
+        // Récupérer tous les quiz
+        // $allQuizzes = $quizzesRepository->findAll();
+
+        // Récupérer les vidéos
+        $videos = $this->YouTubeScriptService->getLatestVideos(4);
+
+        return $this->render('quiz/index.html.twig', [
+            'controller_name' => 'QuizController',
+            'videos' => $videos,
+            // 'allQuizzes' => $allQuizzes,
+        ]);
     }
 }
