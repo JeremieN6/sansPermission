edb204e15f53b9254b3fcac10dfe4e7524b64bf1
Jérémie NGOYI <47412701+JeremieN6@users.noreply.github.com>
2025-06-11 13:23:18 +0000

    Refactor components for improved styling and structure
    - Updated hosts carousel to use a section layout with enhanced styling and responsive design.
    - Redesigned podcast banner for better alignment and visual appeal.
    - Revamped stats component to improve layout and readability, ensuring better presentation of channel statistics.
    - Enhanced testimonials section with a more structured layout and improved accessibility.
    - Overhauled footer design for better organization of links and improved responsiveness.

--- src/Form/CollaborationType.php
+++ src/Form/CollaborationType.php
@@ -58,7 +58,7 @@ class CollaborationType extends AbstractType
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Envoyer',
-                'attr' => ['class' => 'text-white bg-[#4ADE80] hover:bg-[#22c55e] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'],
+                'attr' => ['class' => 'w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'],
             ]);
     }
 
